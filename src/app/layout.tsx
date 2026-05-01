d4a227395aa241d330f624fe4e37d6b6bb5c3bfc
chuluosen <chuluosen4869@gmail.com>
2026-05-01 14:37:52 +0800

    Add AdSense verification script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3634547302640680"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-DZCSFZGPRT" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
